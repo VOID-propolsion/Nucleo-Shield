1ba3dd129a2f72b20ba4b5c43d06c3600cc3a649
Mat A Door <62701878+MatheusTran@users.noreply.github.com>
2024-05-03 22:37:22 +0200

    added back old HAL modules

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -57,11 +57,15 @@
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+/**
 /**
   * Initializes the Global MSP.
   */
 void HAL_MspInit(void)
 {
+
 
   /* USER CODE BEGIN MspInit 0 */
 
@@ -69,6 +73,8 @@ void HAL_MspInit(void)
 
   __HAL_RCC_SYSCFG_CLK_ENABLE();
   __HAL_RCC_PWR_CLK_ENABLE();
+
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
 
   HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
 
@@ -81,7 +87,9 @@ void HAL_MspInit(void)
 
 /**
 * @brief UART MSP Initialization
+* @brief UART MSP Initialization
 * This function configures the hardware resources used in this example
+* @param huart: UART handle pointer
 * @param huart: UART handle pointer
 * @retval None
 */
@@ -89,25 +97,37 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
   if(huart->Instance==USART2)
+  if(huart->Instance==USART2)
   {
+  /* USER CODE BEGIN USART2_MspInit 0 */
   /* USER CODE BEGIN USART2_MspInit 0 */
 
   /* USER CODE END USART2_MspInit 0 */
+  /* USER CODE END USART2_MspInit 0 */
     /* Peripheral clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
     __HAL_RCC_USART2_CLK_ENABLE();
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**USART2 GPIO Configuration
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
     */
+    GPIO_InitStruct.Pin = USART_TX_Pin | USART_RX_Pin;
     GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /* USER CODE BEGIN USART2_MspInit 1 */
+
+  /* USER CODE END USART2_MspInit 1 */
   /* USER CODE BEGIN USART2_MspInit 1 */
 
   /* USER CODE END USART2_MspInit 1 */
@@ -117,18 +137,24 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 
 /**
 * @brief UART MSP De-Initialization
+* @brief UART MSP De-Initialization
 * This function freeze the hardware resources used in this example
+* @param huart: UART handle pointer
 * @param huart: UART handle pointer
 * @retval None
 */
 void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 {
   if(huart->Instance==USART2)
+  if(huart->Instance==USART2)
   {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
   /* USER CODE BEGIN USART2_MspDeInit 0 */
 
   /* USER CODE END USART2_MspDeInit 0 */
+  /* USER CODE END USART2_MspDeInit 0 */
     /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
     __HAL_RCC_USART2_CLK_DISABLE();
 
     /**USART2 GPIO Configuration
@@ -138,7 +164,15 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
 
   /* USER CODE BEGIN USART2_MspDeInit 1 */
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
 
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+  /* USER CODE END USART2_MspDeInit 1 */
   /* USER CODE END USART2_MspDeInit 1 */
   }
 
