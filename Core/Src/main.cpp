7b6c8dffc5cc410177660329a2b70868c44fe0b5
Mat A Door <62701878+MatheusTran@users.noreply.github.com>
2024-05-04 10:24:22 +0200

    flashing LED test

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,6 +1,8 @@
 #include "stm32f4xx_hal.h"
 
 #define LED_PIN                     GPIO_PIN_5
+#define BOARD_LED_15                GPIO_PIN_15
+#define BOARD_LED_1                 GPIO_PIN_1
 #define LED_GPIO_PORT               GPIOA
 #define LED_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOA_CLK_ENABLE()
 
@@ -11,15 +13,34 @@ int main(void) {
 
     GPIO_InitTypeDef GPIO_InitStruct;
 
-    GPIO_InitStruct.Pin     = LED_PIN;
+    // Initialize pin 5 (LED_PIN)
     GPIO_InitStruct.Mode    = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull    = GPIO_PULLUP;
     GPIO_InitStruct.Speed   = GPIO_SPEED_HIGH;
 
+    GPIO_InitStruct.Pin     = LED_PIN;
     HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 
+    // Initialize pin 15 (BOARD_LED)
+    GPIO_InitStruct.Pin     = BOARD_LED_15;
+    HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+
+    // Initialize pin 1 (BOARD_LED)
+    GPIO_InitStruct.Pin     = BOARD_LED_1;
+    HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+    int count = 0;
     while (1) {
-        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-        HAL_Delay(1000);
+        // Toggle pin 5
+        if (count % 2) HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+        
+        // Toggle pin 15
+        if (count % 3 == 0) HAL_GPIO_TogglePin(LED_GPIO_PORT, BOARD_LED_15);
+
+        // Toggle pin 1
+        if (count % 5 == 0) HAL_GPIO_TogglePin(LED_GPIO_PORT, BOARD_LED_1);
+
+        count++;
+        count = count %5;
+        HAL_Delay(250);
     }
 }
--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -76,8 +76,6 @@ void HAL_MspInit(void)
 
   HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
 
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-
   /* System interrupt init*/
 
   /* USER CODE BEGIN MspInit 1 */
@@ -87,9 +85,7 @@ void HAL_MspInit(void)
 
 /**
 * @brief UART MSP Initialization
-* @brief UART MSP Initialization
 * This function configures the hardware resources used in this example
-* @param huart: UART handle pointer
 * @param huart: UART handle pointer
 * @retval None
 */
@@ -100,34 +96,23 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
   if(huart->Instance==USART2)
   {
   /* USER CODE BEGIN USART2_MspInit 0 */
-  /* USER CODE BEGIN USART2_MspInit 0 */
 
   /* USER CODE END USART2_MspInit 0 */
-  /* USER CODE END USART2_MspInit 0 */
     /* Peripheral clock enable */
-    __HAL_RCC_USART2_CLK_ENABLE();
     __HAL_RCC_USART2_CLK_ENABLE();
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**USART2 GPIO Configuration
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
     */
     GPIO_InitStruct.Pin = USART_TX_Pin | USART_RX_Pin;
-    GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN USART2_MspInit 1 */
-
-  /* USER CODE END USART2_MspInit 1 */
   /* USER CODE BEGIN USART2_MspInit 1 */
 
   /* USER CODE END USART2_MspInit 1 */
@@ -137,31 +122,19 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 
 /**
 * @brief UART MSP De-Initialization
-* @brief UART MSP De-Initialization
 * This function freeze the hardware resources used in this example
-* @param huart: UART handle pointer
 * @param huart: UART handle pointer
 * @retval None
 */
 void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 {
   if(huart->Instance==USART2)
-  if(huart->Instance==USART2)
   {
   /* USER CODE BEGIN USART2_MspDeInit 0 */
-  /* USER CODE BEGIN USART2_MspDeInit 0 */
 
-  /* USER CODE END USART2_MspDeInit 0 */
   /* USER CODE END USART2_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_USART2_CLK_DISABLE();
-    __HAL_RCC_USART2_CLK_DISABLE();
-
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
-    HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
 
   /* USER CODE BEGIN USART2_MspDeInit 1 */
     /**USART2 GPIO Configuration
@@ -172,7 +145,6 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 
   /* USER CODE BEGIN USART2_MspDeInit 1 */
 
-  /* USER CODE END USART2_MspDeInit 1 */
   /* USER CODE END USART2_MspDeInit 1 */
   }
 
