d3635bbc28bb0df9f83cdd82c917fec2cde9bc20
Mat A Door <62701878+MatheusTran@users.noreply.github.com>
2024-05-05 08:41:28 +0200

    updated cmake to include all libraries

--- app/Inc/app.hpp
+++ app/Inc/app.hpp
@@ -1,6 +1,17 @@
 #pragma once
+#define BOARD_BUTTON        GPIO_PIN_13 // b
+#define BOARD_LED           GPIO_PIN_5 // a
+
+#define SHIELD_LED_1        GPIO_PIN_15 // a
+#define SHIELD_LED_2        GPIO_PIN_1 // a
+
+#define SHIELD_SWITCH_1     GPIO_PIN_8 // C
+#define SHIELD_SWITCH_2     GPIO_PIN_7 // C
+#define SHIELD_SWITCH_3     GPIO_PIN_5 // C
+#define SHIELD_SWITCH_4     GPIO_PIN_4 // C
 
 #ifdef __cplusplus
+
 extern "C"
 {
 #endif
--- app/Src/app.cpp
+++ app/Src/app.cpp
@@ -1,19 +1,33 @@
 #include "app.hpp"
 #include "main.h"
+#include "RadioLib.h"
+
+int switches[4] = {SHIELD_SWITCH_1, SHIELD_SWITCH_2, SHIELD_SWITCH_3, SHIELD_SWITCH_4};
 
 extern "C"
 {
-
     void setup()
     {
     }
 
     void loop()
     {
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
-        HAL_Delay(1000);
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+        if (!HAL_GPIO_ReadPin(GPIOC, BOARD_BUTTON)) {
+            for (int i = 0; i < 4; i++) {
+                HAL_GPIO_WritePin(GPIOA, SHIELD_LED_2, GPIO_PIN_SET);
+                if (!HAL_GPIO_ReadPin(GPIOC, switches[i])) {
+                    HAL_GPIO_WritePin(GPIOA, SHIELD_LED_1, GPIO_PIN_SET);
+                }
+                HAL_Delay(250);
+                HAL_GPIO_WritePin(GPIOA, SHIELD_LED_1, GPIO_PIN_RESET);
+                HAL_GPIO_WritePin(GPIOA, SHIELD_LED_2, GPIO_PIN_RESET);
+                HAL_Delay(250);
+            }
+            while (!HAL_GPIO_ReadPin(GPIOC, BOARD_BUTTON)) {
+                HAL_Delay(100);
+            }
+        }
+
         HAL_Delay(100);
     }
 }
